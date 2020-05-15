3484627efbdc0ecb0debe89dd20b74b34921d135
Cândea Cătălin <catalin.candea@student.upt.ro>
2020-05-15 23:02:18 +0300

    Added basic database package and resources

---
+++ src/main/java/Database/DiskDB.java
@@ -0,0 +1,27 @@
+package Database;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import Order.Product;
+
+public class DiskDB {
+	private ArrayList<Product> products = new ArrayList<Product>();
+
+	public ArrayList<Product> getProductsfromDisk() {
+		ObjectMapper objectMapper = new ObjectMapper();
+
+		return products;
+	}
+
+	public void writeProductsToDisk(ArrayList<Product> products)
+			throws JsonGenerationException, JsonMappingException, IOException {
+		ObjectMapper objectMapper = new ObjectMapper();
+		objectMapper.writeValue(new File("/CarCosCan-Livreisan/src/test/resources/Database/products.json"), products);
+	}
+}
